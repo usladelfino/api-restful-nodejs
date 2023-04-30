23a43a15466558d661fc775d69b704f209ce7aa0
usladelfino <40376796+usladelfino@users.noreply.github.com>
2023-04-30 19:38:19 -0300

    Documentação do código-fonte

--- controller.js
+++ controller.js
@@ -1,9 +1,36 @@
+/**
+
+@typedef Produto
+@property {number} id - O id do produto.
+@property {string} descricao - A descrição do produto.
+@property {number} valor - O valor do produto.
+@property {string} marca - A marca do produto.
+*/
 const lista_produtos = require('./server').lista_produtos;
 
+
+
+/**
+
+Retorna todos os produtos da lista.
+@function getProdutos
+@param {Object} req - Objeto com a requisição HTTP.
+@param {Object} res - Objeto com a resposta HTTP.
+@returns {Object} - Lista com todos os produtos.
+*/
 exports.getProdutos = (req, res) => {
     res.json(lista_produtos.produtos);
 };
 
+
+/**
+
+Retorna um produto específico com base no seu ID.
+@function getProdutoById
+@param {Object} req - Objeto com a requisição HTTP.
+@param {Object} res - Objeto com a resposta HTTP.
+@returns {Produto} - Produto encontrado.
+*/
 exports.getProdutoById = (req, res) => {
     const produto = lista_produtos.produtos.find(p => p.id === parseInt(req.params.id));
     if (!produto) {
@@ -12,6 +39,15 @@ exports.getProdutoById = (req, res) => {
     res.json(produto);
 };
 
+
+/**
+
+Cria um novo produto na lista de produtos.
+@function createProduto
+@param {Object} req - Objeto com a requisição HTTP.
+@param {Object} res - Objeto com a resposta HTTP.
+@returns {Produto} - Produto criado.
+*/
 exports.createProduto = (req, res) => {
     const produto = {
         id: lista_produtos.produtos.length + 1,
@@ -23,6 +59,15 @@ exports.createProduto = (req, res) => {
     res.json(produto);
 };
 
+
+/**
+
+Atualiza as informações de um produto específico com base no seu ID.
+@function updateProduto
+@param {Object} req - Objeto com a requisição HTTP.
+@param {Object} res - Objeto com a resposta HTTP.
+@returns {Produto} - Produto atualizado.
+*/
 exports.updateProduto = (req, res) => {
     const produto = lista_produtos.produtos.find(p => p.id === parseInt(req.params.id));
     if (!produto) {
@@ -34,6 +79,15 @@ exports.updateProduto = (req, res) => {
     res.json(produto);
 };
 
+
+/**
+
+Deleta um produto específico com base no seu ID.
+@function deleteProduto
+@param {Object} req - Objeto com a requisição HTTP.
+@param {Object} res - Objeto com a resposta HTTP.
+@returns {Object} - Lista atualizada de produtos após a exclusão.
+*/
 exports.deleteProduto = (req, res) => {
     const index = lista_produtos.produtos.findIndex(p => p.id === parseInt(req.params.id));
     if (index === -1) {
--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+/**
+
+@fileOverview Este módulo é responsável por gerenciar a API de produtos.
+@module produtos
+@requires express
+@requires body-parser
+*/
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -14,12 +22,32 @@ const lista_produtos = {
     ]
 };
 
-// GET /produtos
+/**
+
+Retorna todos os produtos disponíveis.
+@name getProdutos
+@function
+@memberof module:produtos
+@inner
+@param {Object} req - Objeto de requisição do Express.
+@param {Object} res - Objeto de resposta do Express.
+@returns {Array} - Um array de objetos de produtos.
+*/
 app.get('/produtos', (req, res) => {
     res.json(lista_produtos.produtos);
 });
+/**
 
-// GET /produtos/:id
+Retorna um produto pelo seu ID.
+@name getProdutoById
+@function
+@memberof module:produtos
+@inner
+@param {Object} req - Objeto de requisição do Express.
+@param {Object} res - Objeto de resposta do Express.
+@returns {Object} - Um objeto de produto.
+@throws {string} - Retorna uma mensagem de erro caso o produto não seja encontrado.
+*/
 app.get('/produtos/:id', (req, res) => {
     const produto = lista_produtos.produtos.find(p => p.id === parseInt(req.params.id));
     if (!produto) {
@@ -27,8 +55,17 @@ app.get('/produtos/:id', (req, res) => {
     }
     res.json(produto);
 });
+/**
 
-// POST /produtos
+Cria um novo produto.
+@name createProduto
+@function
+@memberof module:produtos
+@inner
+@param {Object} req - Objeto de requisição do Express.
+@param {Object} res - Objeto de resposta do Express.
+@returns {Object} - Um objeto de produto.
+*/
 app.post('/produtos', (req, res) => {
     const produto = {
         id: lista_produtos.produtos.length + 1,
@@ -39,8 +76,18 @@ app.post('/produtos', (req, res) => {
     lista_produtos.produtos.push(produto);
     res.json(produto);
 });
+/**
 
-// PUT /produtos/:id
+Atualiza um produto pelo seu ID.
+@name updateProdutoById
+@function
+@memberof module:produtos
+@inner
+@param {Object} req - Objeto de requisição do Express.
+@param {Object} res - Objeto de resposta do Express.
+@returns {Object} - Um objeto de produto atualizado.
+@throws {string} - Retorna uma mensagem de erro caso o produto não seja encontrado.
+*/
 app.put('/produtos/:id', (req, res) => {
     const produto = lista_produtos.produtos.find(p => p.id === parseInt(req.params.id));
     if (!produto) {
@@ -52,7 +99,17 @@ app.put('/produtos/:id', (req, res) => {
     res.json(produto);
 });
 
-// DELETE /produtos/:id
+/**
+
+Deleta um produto com o ID especificado.
+@function
+@memberof module:produtos
+@name deleteProduto
+@param {object} req - Objeto de requisição HTTP.
+@param {object} res - Objeto de resposta HTTP.
+@returns {string} - Mensagem de sucesso se o produto for removido com sucesso.
+@throws {Error} - Retorna um erro se o ID do produto não for encontrado.
+*/
 app.delete('/produtos/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const index = lista_produtos.produtos.findIndex(p => p.id === id);
@@ -60,15 +117,22 @@ app.delete('/produtos/:id', (req, res) => {
         return res.status(404).send('Produto não encontrado');
     }
     lista_produtos.produtos.splice(index, 1);
-    res.send(`Produto ${id} removido com sucesso.`);
+    res.send(`Produto ${ id } removido com sucesso.`);
 });
 
+/**
+ 
+Inicia o servidor da aplicação na porta definida na variável de ambiente "PORT" ou na porta 3000.
+@function
+@memberof module:produtos
+@name startServer
+@returns {void}
+*/
 if (process.env.NODE_ENV !== 'test') {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-        console.log(`Servidor iniciado na porta ${PORT}`);
+        console.log(`Servidor iniciado na porta ${ PORT }`);
     });
 }
-
 
 module.exports = app;
