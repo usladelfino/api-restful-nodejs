689320d3d12a8d1255427b77586d602fcca728e4
usladelfino <40376796+usladelfino@users.noreply.github.com>
2023-05-02 23:24:21 -0300

    conexão com o banco de dados para persistencia

--- src/models/produto.js
+++ src/models/produto.js
@@ -1,42 +1,29 @@
-/**
- * Classe que representa um produto.
- * @class
- * @classdesc Um produto com ID, descrição, valor e marca.
- * @param {number} id - O ID do produto.
- * @param {string} descricao - A descrição do produto.
- * @param {number} valor - O valor do produto.
- * @param {string} marca - A marca do produto.
- */
-class Produto {
-    constructor(id, descricao, valor, marca) {
-        this.id = id;
-        this.descricao = descricao;
-        this.valor = valor;
-        this.marca = marca;
+const db = require('../repository/database');
+
+const produtoModel = {
+    getProdutos: async () => {
+        const produtos = await db('produto').select('*');
+        return produtos;
+    },
+    getProdutoById: async (id) => {
+        const produto = await db('produto').where({ id }).select('*').first();
+        return produto;
+    },
+    createProduto: async (produtoData) => {
+        const [produtoId] = await db('produto').insert(produtoData).returning("id");
+        const produto = await db('produto').where({id : produtoId.id}).select('*').first();
+        return produto;
+    },
+    updateProduto: async (id, produtoData) => {
+        await db('produto').where({ id }).update(produtoData);
+        const produto = await db('produto').where({ id }).select('*').first();
+        return produto;
+    },
+    deleteProduto: async (id) => {
+        const produto = await db('produto').where({ id }).select('*').first();
+        await db('produto').where({ id }).delete();
+        return produto;
     }
 }
 
-/**
- * Objeto contendo a lista de produtos.
- * @typedef {Object} ListaProdutos
- * @property {Array<Produto>} produtos - Array de produtos.
- */
-
-/**
- * Objeto que representa uma lista de produtos.
- * @type {ListaProdutos}
- */
-const lista_produtos = {
-    produtos: [
-        { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João" },
-        { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans" },
-        { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé" },
-        { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps" },
-        { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé" },
-    ]
-};
-
-module.exports = {
-    Produto,
-    lista_produtos,
-};
+module.exports = produtoModel;
