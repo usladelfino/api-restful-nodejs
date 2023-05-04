a02f67a4349862ad4c33ed984715edc1c009cec9
usladelfino <40376796+usladelfino@users.noreply.github.com>
2023-05-03 23:57:20 -0300

    utilizando github secrets nas variaveis de conexão com o BD

--- src/repository/database.js
+++ src/repository/database.js
@@ -22,12 +22,14 @@ require('dotenv').config();
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: process.env.DB_HOST || secrets.DB_PORT,
+    port: process.env.DB_PORT || secrets.DB_PORT,
+    user: process.env.DB_USER || secrets.DB_USER,
+    password: process.env.DB_PASSWORD || secrets.DB_PASSWORD,
+    database: process.env.DB_DATABASE || secrets.DB_DATABASE
   }
 });
+
+console.log('The value of the DB_PASSWORD secret is:', process.env.DB_PASSWORD || secrets.DB_PASSWORD);
 
 module.exports = db;
