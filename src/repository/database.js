be5c678ae5a0e768ff2748d78eb63803a8c4360f
usladelfino <40376796+usladelfino@users.noreply.github.com>
2023-05-13 21:37:22 -0300

    mudança do banco de dados para mysql

--- src/repository/database.js
+++ src/repository/database.js
@@ -20,10 +20,9 @@ require('dotenv').config();
  * @property {string} connection.database - Nome do banco de dados a ser utilizado.
  */
 const db = knex({
-  client: 'pg',
+  client: 'mysql2',
   connection: {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
